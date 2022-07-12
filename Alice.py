85c9ed6fb5fc3a127c9809aa5498c9c03f79dc0a
SimoneFerrari99 <simone99ferrari99@gmail.com>
2022-07-12 15:46:06 +0200

    Simple connection and communication with RSA

--- Alice.py
+++ Alice.py
@@ -1,6 +1,7 @@
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
+import socket
 
 # key = RSA.generate(2048)
 # private_key = key.export_key()
@@ -9,25 +10,44 @@ from Crypto.Cipher import AES, PKCS1_OAEP
 # file_out.close()
 
 # public_key = key.publickey().export_key()
+# # hash_object = hashlib.sha1(public_key)
+# # hex_digest = hash_object.hexdigest()
 # file_out = open("AlicePub.pem", "wb")
 # file_out.write(public_key)
 # file_out.close()
 
+# Crea il socket "stream based" basato sul protocollo TCP ed indirizzi IPv4
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connetto il client al server
+print("> Connessione... ")  
 
+ 
+clientSocket.connect(("127.0.0.1",9090))
+print( "> Connessione eseguita" )
 
+# Recupero chiave pubblica di Bob
+Bpbk = RSA.import_key(clientSocket.recv(2048))
 
-data = "I met aliens in UFO. Here is the map.".encode("utf-8")
-file_out = open("encrypted_data.bin", "wb")
+clientSocket.send("OK".encode("utf-8"));
 
-recipient_key = RSA.import_key(open("BobPub.pem").read())
 session_key = get_random_bytes(16)
 
 # Encrypt the session key with the public RSA key
-cipher_rsa = PKCS1_OAEP.new(recipient_key)
+cipher_rsa = PKCS1_OAEP.new(Bpbk)
 enc_session_key = cipher_rsa.encrypt(session_key)
+
+data = "Hello Word".encode("utf-8")
+file_out = open("encrypted_data.bin", "wb")
 
 # Encrypt the data with the AES session key
 cipher_aes = AES.new(session_key, AES.MODE_EAX)
 ciphertext, tag = cipher_aes.encrypt_and_digest(data)
 [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
 file_out.close()
+clientSocket.send("END".encode("utf-8"))
+
+
+
+
+
+
--- Bob.py
+++ Bob.py
@@ -1,27 +1,57 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
+import socket
 
-# key = RSA.generate(2048)
-# private_key = key.export_key()
+# import hashlib
+
+key = RSA.generate(2048)
+Bpvtk = key.export_key()
 # file_out = open("BobPvt.pem", "wb")
-# file_out.write(private_key)
+# file_out.write(Bpvtk)
 # file_out.close()
 
-# public_key = key.publickey().export_key()
+Bpbk = key.publickey().export_key()
+# hash_object = hashlib.sha1(public_key)
+# hex_digest = hash_object.hexdigest()
 # file_out = open("BobPub.pem", "wb")
 # file_out.write(public_key)
 # file_out.close()
 
 
+bobSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Binding del socket e attesa di connessione da parte del client
+bobSocket.bind(("127.0.0.1",9090))
+print("> In attesa di connessione...")
+bobSocket.listen()
+
+# Avvenuta connessione da parte del processo client
+(clientConnected, clientAddress) = bobSocket.accept()
+print("  >> Client connesso. (%s:%s)" % (clientAddress[0], clientAddress[1]))
+
+# Invio chiave pubblica di Bob
+clientConnected.send(Bpbk)
+
+response = clientConnected.recv(2048).decode("utf-8")
+
+if response == "OK":
+    print("  >> Chiave pubblica di Bob inviata correttamente.")
+
+endWriting = clientConnected.recv(2048).decode("utf-8")
+if endWriting == "END":
+    print("  >> Ricevuto messaggio di fine scrittura.")
+    bobSocket.close()
+    print("  >> Connessione chiusa.")
+
 file_in = open("encrypted_data.bin", "rb")
+print("  >> Apertura file encrypted_data.bin")
 
-private_key = RSA.import_key(open("BobPvt.pem").read())
+Bpvtk = RSA.import_key(Bpvtk)
 
-enc_session_key, nonce, tag, ciphertext = \
-   [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+enc_session_key, nonce, tag, ciphertext = [ file_in.read(x) for x in (Bpvtk.size_in_bytes(), 16, 16, -1) ]
 
 # Decrypt the session key with the private RSA key
-cipher_rsa = PKCS1_OAEP.new(private_key)
+cipher_rsa = PKCS1_OAEP.new(Bpvtk)
 session_key = cipher_rsa.decrypt(enc_session_key)
 
 # Decrypt the data with the AES session key
